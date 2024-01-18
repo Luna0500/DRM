36a09db69f001f6d77abd6ef9c7b1f902a53fa7f
Ryan Lunas <luna0500@bears.unco.edu>
2024-01-17 18:00:58 -0700

    Config to allow remote image display, Basic List page and JSON Parsing

---
+++ Project/app/list/page.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+import * as fs from 'fs';
+
+export default function Page() {
+    let rawData = fs.readFileSync('../Project/public/sampleCards.json', 'utf8');
+    let jsonDataArray = JSON.parse(rawData);
+    const imgArray = [];
+    for (var i = 0; i < jsonDataArray.length; i++) {
+        imgArray[i] = jsonDataArray[i].images.small;
+    }
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1>List Page!</h1>
+            <div className="flex min-h-screen flex-wrap items-center justify-between p-24">
+
+            {imgArray.map((imageSrc, index) => (
+                <Image
+                    key={index}
+                    className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+                    src={imageSrc}
+                    alt={`Image ${index}`}
+                    width={180}
+                    height={37}
+                    priority
+                />
+            ))}
+            </div>
+        </main>
+    );
+}
--- Project/next.config.js
+++ Project/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.pokemontcg.io',
+                port: '',
+            },
+        ],
+    },
+}
 
 module.exports = nextConfig
