e8ec6e9a69102f6585ba68ccadf37b0c42286a2a
luna0500 <luna0500@KEP-0075-03-21>
2024-01-25 18:23:55 -0700

    extra card data

--- Project/app/dbtest/page.tsx
+++ Project/app/dbtest/page.tsx
@@ -1,17 +1,14 @@
 import Image from 'next/image';
 import { PokemonTCG } from 'pokemon-tcg-sdk-typescript';
 
-async function fetchCardData() {
-    const card = await PokemonTCG.findCardByID("xy1-4");
-    return card;
-}
-
 export default async function ServerComponent() {
-    const card = await fetchCardData();
+    const card = await PokemonTCG.findCardByID("xy1-7");
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <h1>DB Test Page!</h1>
+            <h2>{card.name}</h2>
+            <h2>{card.flavorText}</h2>
             <div className="flex min-h-screen flex-wrap items-center justify-between p-24">
                 <Image
                     className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
