51bf7370f14b1164c883929551104a548a294361
DSantander013 <sant8867@bears.unco.edu>
2024-02-22 18:29:02 -0700

    search bar v4

--- Project/app/ui/homeSearch.tsx
+++ Project/app/ui/homeSearch.tsx
@@ -1,8 +1,14 @@
 'use client';
-
+import { Fragment } from 'react'
+import { Menu, Transition } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/24/solid'
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useState } from 'react';
+
+
+
+
 
 export default function HomeSearch({ placeholder }: { placeholder: string }) {
     const searchParams = useSearchParams();
@@ -24,9 +30,38 @@ export default function HomeSearch({ placeholder }: { placeholder: string }) {
         e.preventDefault();
         handleSearch(searchTerm);
     };
-
-    return (
-        <form onSubmit={handleSubmit} className="relative flex flex-1 flex-shrink-0">
+    
+        return (
+            <div>
+            <Menu>
+                <Menu.Button>More</Menu.Button>
+                <Menu.Items>
+                    <Menu.Item>
+                        {({ active }) => (
+                            <a
+                                className={`${active && 'bg-blue-500'}`}
+                                href="/account-settings"
+                            >
+                                Account settings
+                            </a>
+                        )}
+                    </Menu.Item>
+                    <Menu.Item>
+                        {({ active }) => (
+                            <a
+                                className={`${active && 'bg-blue-500'}`}
+                                href="/account-settings"
+                            >
+                                Documentation
+                            </a>
+                        )}
+                    </Menu.Item>
+                    <Menu.Item disabled>
+                        <span className="opacity-75">Invite a friend (coming soon!)</span>
+                    </Menu.Item>
+                </Menu.Items>
+            </Menu>
+            <form onSubmit={handleSubmit} className="relative flex flex-1 flex-shrink-0">
             <label htmlFor="search" className="sr-only">
                 Search
             </label>
@@ -38,6 +73,10 @@ export default function HomeSearch({ placeholder }: { placeholder: string }) {
                 value={searchTerm}
             />
             <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            
         </form>
-    );
+            </div>
+        )
+    
+    
 }
