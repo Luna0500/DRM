b8e5922c4455b3f0c93ec60fed38ec735d7f08f8
luna0500 <luna0500@KEP-0075-03-21>
2024-03-21 18:31:18 -0600

    Logo Update

--- Project/app/ui/header.tsx
+++ Project/app/ui/header.tsx
@@ -3,7 +3,7 @@
 import { useSession, signIn, signOut } from "next-auth/react"
 import '@/app/globals.css'
 import Image from 'next/image';
-import logo from '@/public/logo.png';
+import logo from '@/public/logo_by_jormxdos.png';
 import '@/app/cardlist/homePage.css'
 
 export default function Header() {
@@ -12,7 +12,9 @@ export default function Header() {
 
     return (
         <div className="fixed top-0 left-0 right-0 flex justify-between items-center h-20 bg-blue-900 text-white px-8 z-30">
-            <Image src={logo} alt="logo" className="w-16 h-16" />
+            <a href="/" className="mr-4">
+                <Image src={logo} alt="logo" className="w-16 h-16" />
+            </a>
             <p className="font-mono text-sm">
                 Welcome to DRM Name TBD
             </p>
