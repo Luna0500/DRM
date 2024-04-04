59524976bedc4ef65f8e82d3d1cff825ab1be9c8
DSantander013 <sant8867@bears.unco.edu>
2024-04-04 02:13:05 -0600

    homepage

--- Project/app/page.tsx
+++ Project/app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Image from 'next/image'
 import HomeSearch from '@/app/ui/homeSearch'
 import fs from 'fs';
@@ -6,28 +8,38 @@ import Search from '@/app/ui/search';
 import { fetchCardsByName, fetchCardsByAttack, fetchCardsByHP } from '@/app/lib/data';
 import React from 'react';
 import heropic from '@/public/zygardehero.jpg'
+import { useSession, signIn, signOut } from "next-auth/react"
 export default function Home() {
-
+    const { data: session, status } = useSession()
+    const userEmail = session?.user?.email
 
   return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[#daffb5]">
-          
-      
-          <div>
-              <h1>Welcome to Zycardes</h1>
-              <p>To get started click on the button below</p>
-              <Image
-                  unoptimized
-                  src={heropic}
-                  alt="heropic"         
-              width={400}
-              height={500}
-              
-              priority
-              className="rounded-md p-4"
-                   />
-          </div>
-                              
+          <div className="border-hp">
+              <div className="flex justify-between">
+                  <div>  <h1 className="text-4xl flex" >Welcome to Zycardes</h1>
+                      <div className="divide-y">
+                          <p className="flex">Welcome to our e-commerce website to buy, view and sell cards. To get started click on the button below</p>
+                          {status === "authenticated" ? (
+                              <>
+                                  <p>Signed in as {userEmail + "- -"}</p>
+                                  <button className="topbar-buttons mr-4" onClick={() => signOut()}>Sign out</button>
+                              </>
+                          ) : (
+                              <button className="topbar-buttons mr-4" onClick={() => signIn("GitHub")}>Sign in</button>
+                          )}
+                      </div>
+                  </div>            
+                  <Image
+                      unoptimized
+                      src={heropic}
+                      alt="heropic"
+                      width={850}
+                      priority
+                      className="flex"
+                  />
+              </div>    
+          </div>                             
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="/createlisting"
