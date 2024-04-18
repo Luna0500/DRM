5366ed0f3d60ed59bff7259646a3a9bb585e0e14
DSantander013 <sant8867@bears.unco.edu>
2024-04-18 17:56:47 -0600

    account fix

--- Project/app/account/page.tsx
+++ Project/app/account/page.tsx
@@ -1,1 +1,8 @@
-<h1 className="text-5xl text-black">This is your account!</h1>
+export default async function Page(){ 
+
+    return (
+        <main>
+            <h1 className="text-5xl text-black">This is your account!</h1>
+        </main>
+    );
+}
