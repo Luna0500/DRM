f4bf0174327279b2352f4fc1ad522b51573659c9
DSantander013 <sant8867@bears.unco.edu>
2024-04-18 18:18:36 -0600

    delete account

--- Project/app/account/page.tsx
+++ Project/app/account/page.tsx
@@ -1,8 +1,15 @@
-export default async function Page(){ 
+import heropic from '@/public/zygardehero.jpg'
+import Image from 'next/image'
+export default function Page() { 
 
     return (
-        <main>
+        <main className="colorbg flex min-h-screen flex-col items-center justify-between p-24">
             <h1 className="text-5xl text-black">This is your account!</h1>
+            <div className="bg-white border border-black p-4 mb-8">
+            <button className="bg-red-700 hover:bg-red-800 text-2xl">              
+                Delete Account
+                </button>
+            </div>
         </main>
-    );
+    )
 }
