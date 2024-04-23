9a1316e578b80f05a3080021b43ad1cdf09c0180
DSantander013 <sant8867@bears.unco.edu>
2024-04-23 17:53:49 -0600

    buy button again...

--- Project/app/cart/page.tsx
+++ Project/app/cart/page.tsx
@@ -41,9 +41,10 @@ export default async function Page() {
         <main className="colorbg flex min-h-screen flex-col items-center justify-between p-24 ">
 
             <h1 className="text-5xl text-black">Shopping Cart</h1>
+            <div>
             <DisplayCartListings listings={listings} userEmail={userEmail} />
             <CartBuyButton listings={listings} userEmail={userEmail} />
-
+            </div>
         </main>
     );
 }
